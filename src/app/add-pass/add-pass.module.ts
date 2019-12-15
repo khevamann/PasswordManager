e98b0a015d63657d8262e099cb8ea0fe41e1725d
Kheva <khevamann@gmail.com>
2019-12-14 19:04:50 -0800

    Removed ion-4-autocomplete

--- src/app/add-pass/add-pass.module.ts
+++ src/app/add-pass/add-pass.module.ts
@@ -7,14 +7,12 @@ import { IonicModule } from '@ionic/angular';
 import { AddPassPageRoutingModule } from './add-pass-routing.module';
 
 import { AddPassPage } from './add-pass.page';
-import {AutoCompleteModule} from 'ionic4-auto-complete';
 
 @NgModule({
   imports: [
     CommonModule,
     FormsModule,
     IonicModule,
-    AutoCompleteModule,
     AddPassPageRoutingModule,
   ],
   declarations: [AddPassPage]
